cbc9210394739d56e6cb6bd22961a7798566eb16
LucasMatheus96 <lucasmatheus.martinss@gmail.com>
2025-08-04 22:42:41 -0300

    Criação Usarios e suas rotas

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,9 @@ const app = require('express')()
 const consign = require('consign')
 
 consign()
-    .then('.config/middlewares.js')
+    .then('config/middlewares.js')
+    .then('./api')
+    .then('./config/routes.js')
     .into(app)
 
 app.listen(3000,()=>{
